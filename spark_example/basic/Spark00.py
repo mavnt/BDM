b468eeff6bdb072fb6285681f8cf54b988fde5d3
Mirko Avantaggiato <mirko.avantaggiato@gmail.com>
2018-05-19 17:28:03 +0200

    basic examples, README.md

---
+++ spark_example/basic/Spark00.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+from pyspark import SparkContext
+
+
+def main():
+    sc = SparkContext('local[4]', 'WordCount')
+    sc.setLogLevel("WARN")
+    path0 = "Spark00.py"
+    path1 = "Spark01.py"
+    tf1 = sc.textFile(path0)
+    tf2 = sc.textFile(path1)
+    words = tf1.union(tf2).flatMap(lambda s: s.split(" "))
+    ones = words.map(lambda word: (word, 1))
+    result = ones.reduceByKey(lambda x, y: x + y).collectAsMap()
+    for k in result:
+        print(k, result[k])
+
+
+if __name__ == '__main__':
+    main()
---
+++ spark_example/basic/Spark01.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+from pyspark import SparkContext
+
+
+def main():
+    sc = SparkContext('local[4]', 'WordByLength')
+    sc.setLogLevel("WARN")
+    tf1 = sc.textFile("Spark00.py")
+    words = tf1.flatMap(lambda s: s.split(" "))
+    results = words.map(lambda w: (len(w), 1)).reduceByKey(lambda x, y: x + y).collectAsMap()
+    for k in results:
+        print(k, results[k])
+
+
+if __name__ == '__main__':
+    main()
